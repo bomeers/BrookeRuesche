74ce90f224fac0f64a3c62e106e4ae5781ad0f07
bomeers <bmeers5@gmail.com>
2020-08-31 17:09:30 -0500

    added navbar added categories

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,9 @@
-// window.onload = setBackground(5);
+window.onload = setBackground();
 
-// function setBackground(imgCount) {
-//     var rand = Math.round(Math.random() * (imgCount - 1)) + 1;
-//     var images = ['brfitness-1.jpg', 'brfitness-2.jpg', 'brfitness-3.jpg', 'brfitness-4.jpg', 'brfitness-5.jpg'];
-//     var background = document.getElementById('background');
+function setBackground() {
+    var images = ['brfitness-1.jpg', 'brfitness-2.jpg', 'brfitness-3.jpg', 'brfitness-4.jpg', 'brfitness-5.jpg'];
+    var rand = Math.floor(Math.random() * images.length);
+    var background = document.getElementById('hero-image');
 
-//     background.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(../Images/background/" + images[rand] + ");";
-// }
+    background.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url("./Images/background/${images[rand]}")`;
+}
