5dea01f7d5c1f7190a9733aa40b19015661d46ee
bomeers <bmeers5@gmail.com>
2020-09-01 01:53:37 -0500

    added navbar snapping

--- JS/index.js
+++ JS/index.js
@@ -3,7 +3,11 @@ window.onload = setBackground();
 function setBackground() {
     var images = ['brfitness-1.jpg', 'brfitness-2.jpg', 'brfitness-3.jpg', 'brfitness-4.jpg', 'brfitness-5.jpg'];
     var rand = Math.floor(Math.random() * images.length);
-    var background = document.getElementById('hero-image');
+    var background = document.getElementById('home');
 
     background.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url("./Images/background/${images[rand]}")`;
+}
+
+function snapTo(hash) {
+    location.hash = "#" + hash;
 }
