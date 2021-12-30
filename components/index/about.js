05c43c379d82590d53d3ab671d7330df0b8f1518
bo meers <bmeers5@gmail.com>
2021-12-30 15:56:49 -0600

    - updated folder structure - updated layout of each component - updated bmi page

---
+++ components/index/about.js
@@ -0,0 +1,18 @@
+import styles from '../../styles/home.module.css'
+import { Container, Typography, ThemeProvider, Box } from '@mui/material';
+import theme from '../../public/theme'
+
+export default function About() {
+    return (
+        <Box my={20}>
+            <ThemeProvider theme={theme}>
+                <Container>
+                    <Typography variant='h3' align='left' sx={{ color: 'white' }}>About BRFitness</Typography>
+                    <Typography variant='p' align='left' sx={{ color: 'white' }}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit massa enim nec dui nunc. Ut pharetra sit amet aliquam id diam maecenas ultricies.
+                    </Typography>
+                </Container>
+            </ThemeProvider>
+        </Box>
+    );
+}
--- components/layout.js
+++ components/layout.js
@@ -2,31 +2,9 @@ import Head from 'next/head'
 import { setBackground } from './setBackground'
 import Navbar from './navbar'
 import Footer from './footer'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const name = 'Brooke Ruesche'
 export const siteTitle = 'BR Fitness'
-const BRFtheme = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: {
-            main: '#7b52a0',
-            contrastText: '#fff'
-        },
-        secondary: {
-            main: '#401a51',
-            contrastText: '#fff'
-        },
-        background: {
-            default: '#1f1f1f',
-            paper: '#353434',
-        },
-        text: {
-            primary: '#fff',
-            secondary: '#fff'
-        },
-    },
-});
 
 export default function Layout({ children, home }) {
     return (
@@ -39,25 +17,23 @@ export default function Layout({ children, home }) {
                 />
                 <meta name="og:title" content={siteTitle} />
             </Head>
-            <ThemeProvider theme={BRFtheme}>
-                <header>
-                    {home ? (
-                        <>
-                            <Navbar />
+            <header>
+                {home ? (
+                    <>
+                        <Navbar />
 
-                            <div id="home" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url("images/background/brfitness-${setBackground()}.jpg")` }}>
-                                <img class="center-logo" src="/images/logos/logo-white.svg" />
-                            </div>
-                        </>
-                    ) : (
-                        <>
-                            <Navbar />
-                        </>
-                    )}
-                </header>
-                <main>{children}</main>
-                <Footer />
-            </ThemeProvider>
+                        <div id="home" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url("images/background/brfitness-${setBackground()}.jpg")` }}>
+                            <img class="center-logo" src="/images/logos/logo-white.svg" />
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <Navbar />
+                    </>
+                )}
+            </header>
+            <main>{children}</main>
+            <Footer />
         </div>
     )
 }
