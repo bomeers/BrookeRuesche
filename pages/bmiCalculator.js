eb338b2ecf2553d97e8e1961140505f3b4381652
Brandons404 <brandon531@gmail.com>
2022-01-03 22:26:50 -0600

    Updates BMI page formatting and display

--- pages/bmiCalculator.js
+++ pages/bmiCalculator.js
@@ -1,81 +1,90 @@
-import Head from 'next/head'
-import React, { useState } from "react";
-import Layout, { siteTitle } from '../components/layout.js'
-import { TextField, Button, Box, Grid, InputAdornment } from '@mui/material'
+import Head from 'next/head';
+import React, { useState } from 'react';
+import Layout, { siteTitle } from '../components/layout.js';
+import { TextField, Typography, Button, Box, Grid, InputAdornment } from '@mui/material';
 
 const defaultValues = {
-    feet: "",
-    inches: "",
-    weight: ""
+  feet: '',
+  inches: '',
+  weight: '',
+  bmi: '--',
 };
 
 export default function bmiCalculator() {
+  const [formValues, setFormValues] = useState(defaultValues);
 
-    const [formValues, setFormValues] = useState(defaultValues);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues({ ...formValues, [name]: value, });
-    };
+  const calculateBMI = (event) => {
+    event.preventDefault();
+    const height = Number(formValues.feet) * 12 + Number(formValues.inches);
+    let bmi = (Number(formValues.weight) * 703) / (height * height);
+    bmi = String(bmi.toFixed(2)) === 'NaN' ? '--' : String(bmi.toFixed(2));
+    setFormValues({ ...formValues, bmi });
+    // return BMI
+  };
 
-    const calculateBMI = (event) => {
-        event.preventDefault();
-        var height = +formValues.feet * 12 + +formValues.inches;
-        var BMI = (+formValues.weight * 703) / (height * height);
-        console.log(`BMI Result: ${BMI}`);
-        // return BMI
-    }
-
-    return (
-        <Layout>
-            <Head>
-                <title>BMI Calculator | {siteTitle}</title>
-            </Head>
-            <Box my={20}>
-                <form onSubmit={calculateBMI}>
-                    <Grid container alignItems="center" justifyContent="center" direction="column">
-                        <Grid item>
-                            <TextField
-                                name="feet"
-                                label="Height (feet)"
-                                type="number"
-                                value={formValues.feet}
-                                onChange={handleInputChange}
-                                InputProps={{
-                                    startAdornment: <InputAdornment position="start">ft</InputAdornment>,
-                                }}
-                                sx={{ mx: 1 }}
-                            />
-                            <TextField
-                                name="inches"
-                                label="Height (inches)"
-                                type="number"
-                                value={formValues.inches}
-                                onChange={handleInputChange}
-                                InputProps={{
-                                    startAdornment: <InputAdornment position="start">in</InputAdornment>,
-                                }}
-                                sx={{ mx: 1 }}
-                            />
-                            <TextField
-                                name="weight"
-                                label="Weight"
-                                type="number"
-                                value={formValues.weight}
-                                onChange={handleInputChange}
-                                InputProps={{
-                                    startAdornment: <InputAdornment position="start">lbs</InputAdornment>,
-                                }}
-                                sx={{ mx: 1 }}
-                            />
-                        </Grid>
-                        <Button variant="contained" color="primary" type="submit" sx={{ my: 5 }}>
-                            Submit
-                        </Button>
-                    </Grid>
-                </form>
-            </Box >
-        </Layout >
-    );
-};
-
+  return (
+    <Layout>
+      <Head>
+        <title>BMI Calculator | {siteTitle}</title>
+      </Head>
+      <Box my={20} textAlign='center'>
+        <form onSubmit={calculateBMI}>
+          <Grid container justifyContent='center' spacing={2} sx={{ width: '50%', mx: 'auto' }}>
+            <Grid item xs={12} md={4}>
+              <TextField
+                name='feet'
+                label='Height (feet)'
+                type='number'
+                value={formValues.feet}
+                onChange={handleInputChange}
+                fullWidth
+                InputProps={{
+                  startAdornment: <InputAdornment position='start'>ft</InputAdornment>,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <TextField
+                name='inches'
+                label='Height (inches)'
+                type='number'
+                value={formValues.inches}
+                onChange={handleInputChange}
+                fullWidth
+                InputProps={{
+                  startAdornment: <InputAdornment position='start'>in</InputAdornment>,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <TextField
+                name='weight'
+                label='Weight'
+                type='number'
+                value={formValues.weight}
+                onChange={handleInputChange}
+                fullWidth
+                InputProps={{
+                  startAdornment: <InputAdornment position='start'>lbs</InputAdornment>,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant='contained' color='primary' type='submit' sx={{ my: 5 }}>
+                Submit
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant='h2'>BMI: {formValues.bmi}</Typography>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
+    </Layout>
+  );
+}
--- public/theme.js
+++ public/theme.js
@@ -2,63 +2,63 @@ import { createTheme } from '@mui/material/styles';
 
 // Create a theme instance.
 const BRFtheme = createTheme({
-    palette: {
-        primary: {
-            main: '#7b52a0',
-            contrastText: '#fff'
-        },
-        secondary: {
-            main: '#401a51',
-            contrastText: '#fff'
-        },
-        background: {
-            default: '#1f1f1f',
-            paper: '#353434',
-        },
-        text: {
-            primary: '#fff',
-            secondary: '#fff',
-            disabled: '#9d9d9d'
-        },
+  palette: {
+    primary: {
+      main: '#7b52a0',
+      contrastText: '#fff',
     },
+    secondary: {
+      main: '#401a51',
+      contrastText: '#fff',
+    },
+    background: {
+      default: '#1f1f1f',
+      paper: '#353434',
+    },
+    text: {
+      primary: '#fff',
+      secondary: '#fff',
+      disabled: '#9d9d9d',
+    },
+  },
 
-    components: {
-        MuiTypography: {
-            styleOverrides: {
-                root: {
-                    color: '#fff'
-                }
-            }
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          color: '#fff',
         },
-        MuiSvgIcon: {
-            styleOverrides: {
-                root: {
-                    color: '#fff'
-                }
-            }
+      },
+    },
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          color: '#fff',
         },
-        MuiCalendarPicker: {
-            styleOverrides: {
-                root: {
-                    color: '#fff'
-                }
-            }
+      },
+    },
+    MuiCalendarPicker: {
+      styleOverrides: {
+        root: {
+          color: '#fff',
         },
-        MuiOutlinedInput: {
-            styleOverrides: {
-                notchedOutline: {
-                    borderColor: '#fff'
-                }
-            }
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        notchedOutline: {
+          borderColor: '#fff',
         },
-        MuiFilledInput: {
-            styleOverrides: {
-                root: {
-                    backgroundColor: '#333'
-                }
-            }
-        }
-    }
+      },
+    },
+    MuiFilledInput: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#333',
+        },
+      },
+    },
+  },
 });
 
 export default BRFtheme;
