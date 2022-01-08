a0b90df4b7b0bcfbe6aa52b58e64c52434337d18
Brandons404 <43156510+Brandons404@users.noreply.github.com>
2022-01-08 13:51:58 -0600

    Updates BMI page formatting and display (#1)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
-import { ThemeProvider } from '@emotion/react'
-import theme from '../public/theme'
-import '../styles/globals.css'
+import { ThemeProvider } from '@emotion/react';
+import theme from '../public/theme';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <ThemeProvider theme={theme}> <Component {...pageProps} /> </ThemeProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
