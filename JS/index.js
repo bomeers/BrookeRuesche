9c4d6d6664f929107213199c468b68d1bb5aa7cb
bo meers <bmeers5@gmail.com>
2023-03-09 15:20:39 -0600

    - restored gh-pages

--- JS/index.js
+++
@@ -1,9 +0,0 @@
-window.onload = setBackground();
-
-function setBackground() {
-    var images = ['brfitness-1.jpg', 'brfitness-2.jpg', 'brfitness-3.jpg', 'brfitness-4.jpg', 'brfitness-5.jpg'];
-    var rand = Math.floor(Math.random() * images.length);
-    var background = document.getElementById('background');
-
-    background.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url("./Images/background/${images[rand]}")`;
-}
