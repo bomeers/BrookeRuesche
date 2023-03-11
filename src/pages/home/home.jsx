edc2cbd9849f1b5a46728c7d378584bad5133803
bo meers <bmeers5@gmail.com>
2023-03-11 10:14:36 -0600

    - added weight plate texture - updated weight plate size in blender - overflow set to hidden

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import logo from '/images/logos/logo-white.svg'
 
 // 
@@ -8,19 +10,53 @@ function Home() {
 
     // scene setup
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+
+    // camera setup
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+    const controls = new OrbitControls(camera, renderer.domElement)
+    controls.enableDamping = true
+    controls.target.set(0, 1, 0)
+
+    // create lights
+    const light = new THREE.PointLight()
+    light.position.set(0.8, 1.4, 1.0)
+    scene.add(light)
+
+    const ambientLight = new THREE.AmbientLight()
+    scene.add(ambientLight)
 
     // create box
     // TODO: insert 45lbs weight plate .fbx model to replace cube
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    // scene.add(cube);
 
-    camera.position.z = 5;
+    const fbxLoader = new FBXLoader()
+    fbxLoader.load('blender/weight-plate.fbx', (object) => {
+        // object.scale.divideScalar(100);
+        // object.traverse(function (child) {
+        //     if ((child as THREE.Mesh).isMesh) {
+        //         // (child as THREE.Mesh).material = material
+        //         if ((child as THREE.Mesh).material) {
+        //             ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
+        //         }
+        //     }
+        // })
+        // object.scale.set(.01, .01, .01)
+        scene.add(object)
+    },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log(error)
+        }
+    )
 
     function animate() {
         requestAnimationFrame(animate);
